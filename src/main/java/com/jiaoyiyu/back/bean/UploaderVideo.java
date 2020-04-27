bd679a13849dd01c8e62637d3914600f5b5fd9b4
mmm141023 <925670706@qq.com>
2020-04-27 11:05:30 +0800

    asdasdas

---
+++ src/main/java/com/jiaoyiyu/back/bean/UploaderVideo.java
@@ -0,0 +1,40 @@
+package com.jiaoyiyu.back.bean;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+public class UploaderVideo implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private Integer workId;
+
+    private String url;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getWorkId() {
+        return workId;
+    }
+
+    public void setWorkId(Integer workId) {
+        this.workId = workId;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
